fcf27f58078b5aa49a663f3f16f41cff3b53d6c7
mstanciu552 <mstanciu552@gmail.com>
2021-07-28 14:32:44 +0300

    zsh plugins management

---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.config/zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/zsh-syntax-highlighting
