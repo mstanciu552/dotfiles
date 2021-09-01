726702c82cb011e3ae315bbd7b915cb3166e282c
mstanciu552 <mstanciu552@gmail.com>
2021-09-01 20:32:31 +0300

    fixed backlight

--- i3/switch_workspace.sh
+++ i3/switch_workspace.sh
@@ -3,12 +3,18 @@
 next=$(i3-msg -t get_workspaces | jq -r '.[]| select(.focused).num + 1')
 prev=$(i3-msg -t get_workspaces | jq -r '.[]| select(.focused).num - 1')
 
+if [[ "$next" > 10 ]]
+then
 if [[ $1 == "next" ]] 
 then
   i3-msg workspace number "$next"
 fi
+fi
 
+if [[ "$prev" > 0 ]]
+then
 if [[ $1 == "prev" ]]
 then
   i3-msg workspacenumber "$prev"
+fi
 fi
