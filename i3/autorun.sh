763ff1d29274425306a132feec686189a133e5ff
mstanciu552 <mstanciu552@gmail.com>
2021-09-02 11:28:45 +0300

    added background randomization

--- i3/autorun.sh
+++ i3/autorun.sh
@@ -2,7 +2,8 @@
 
 dunst &
 picom &
-nitrogen --restore
+# nitrogen --restore
+~/.config/i3/wallpaper.sh
 killall -q polybar
 polybar sc0p3 &
 i3-battery-popup -n -D -L 20 -l 10
---
+++ i3/wallpaper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+feh --bg-fill --randomize ~/Pictures/wallpapers/*
+watch -n 1800 feh --bg-fill --randomize ~/Pictures/wallpapers/* &
