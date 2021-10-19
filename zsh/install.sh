dac7a5e833dab63d02303055eb1efe18d8c21922
mstanciu552 <mstanciu552@gmail.com>
2021-10-19 22:56:03 +0300

    added zsh dracula theme and changed to fast-syntax-highlighting

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,4 +2,5 @@
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.config/zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/zsh-syntax-highlighting
+git clone https://github.com/zdharma/fast-syntax-highlighting $HOME/.config/zsh/fast-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
