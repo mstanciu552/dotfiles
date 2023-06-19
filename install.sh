7511d11141c1db147fe5a4c900994876815c9560
Mihai Stanciu <mstanciu552@gmail.com>
2023-06-19 21:04:13 +0300

    (feat) added install script to all config files

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+dirs=$(ls .)
+PWD=$(pwd)
+
+for dir in $dirs; do
+  ln -sf $PWD/$dir $HOME/.config/
+  if [[ -f "$dir/install.sh" ]]; then
+    bash "$dir/install.sh"
+  fi
+done
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.config/zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  $HOME/.config/zsh/zsh-syntax-highlighting
 git clone https://github.com/zdharma/fast-syntax-highlighting $HOME/.config/zsh/fast-syntax-highlighting
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
