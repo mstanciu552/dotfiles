741a305d022236e8fdde00d0f6bb8a7141e0aac8
Mihai Stanciu <mstanciu552@gmail.com>
2025-01-17 09:21:05 +0200

    initial ansible config

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Delete the wallpapers 
-rm -rf ~/Pictures/wallpapers
-
-# Delete all configs that already exist in this repository
-for directory in $(ls); do
-  rm -rf "~/.config/$directory"
-done
